c3cc8f785ec1202f1c740a4d0139ae15cf96ac1e
TradeObjetiva <trade.start@objetivamkt.com.br>
2025-12-15 15:00:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Relatório de Passagem - Objetiva MKT</title>
+    <title>Relatório de Passagem - Agência Objetiva Marketing</title>
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
@@ -556,3 +556,4 @@
 </body>
 
 </html>
+
